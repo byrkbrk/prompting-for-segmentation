af0e1da723aaf7cf229bb0ad06d6cdd4fb68e16b
burak <byrkbrk@gmail.com>
2024-05-20 13:55:47 +0300

    update parse_arguments function

--- segment.py
+++ segment.py
@@ -17,10 +17,16 @@ def parse_arguments():
     parser.add_argument("point_or_bbox_prompts", nargs="+", type=parse_tuple, help="List of point prompts in (height, width) or bbox prompts in (x_min, y_min, x_max, y_max)")
     parser.add_argument("--label_prompts", nargs="+", type=int, default=None, help="List of labels of point prompts")
     parser.add_argument("--image_size", nargs="+", type=int, default=[1024, 1024], help="Size (height, width) to which the image be transformed")
+    parser.add_argument("--checkpoint_name", type=str, default="FastSAM-x.pt", choices=["FastSAM-x.pt", "FastSAM-s.pt"], help="Name of the pretrained model for FastSAM")
+    parser.add_argument("--device", type=str, default=None, help="Name of the device on which the model be run")
     return parser.parse_args()
 
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    PromptSAM(args.image_name).segment(args.point_or_bbox_prompts, args.label_prompts, args.image_size)
+    PromptSAM(args.image_name, 
+              args.checkpoint_name,
+              args.device).segment(args.point_or_bbox_prompts, 
+                                   args.label_prompts, 
+                                   args.image_size)
